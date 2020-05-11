823b3b5cfe3c96518ef35ba49027e3be9e754818
Andreas Sundqvist <andreas@stefna.is>
2020-05-11 14:05:02 +0200

    Add missing class body

--- src/Exceptions/UnknownInputError.php
+++ src/Exceptions/UnknownInputError.php
@@ -2,6 +2,21 @@
 
 namespace Stefna\OpenApiRuntime\Exceptions;
 
-final class UnknownInputError
+use Psr\Http\Message\ResponseInterface;
+
+final class UnknownInputError extends \RuntimeException implements OpenApiResponseException
 {
+	/** @var ResponseInterface */
+	private $response;
+
+	public function __construct(?string $message, ResponseInterface $response)
+	{
+		parent::__construct($message ?? 'Input validation failed in api');
+		$this->response = $response;
+	}
+
+	public function getResponse(): ResponseInterface
+	{
+		return $this->response;
+	}
 }
